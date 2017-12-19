60c474253b3c6ac712999fa6c09068654fef909d
Benjamin Doucet <benjamin.doucet@ynov.com>
2017-12-19 15:53:49 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,17 @@
 exports.run = function(input) {
-  console.log("Exercise #5");
+    var res = 0;
+    var fs = require('fs'),
+        readline = require('readline');
+
+    var rd = readline.createInterface({
+        input: fs.createReadStream(input),
+        output: process.stdout,
+        console: false
+    });
+    rd.on('line', function(line) {
+        console.log(line);
+        res += parseInt(line);
+    });
+    return res;
 };
+    // console.log(res);
