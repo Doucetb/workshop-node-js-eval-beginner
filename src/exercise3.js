ed8c3389a02ff750432a2ccd2166f74e075f94c5
Benjamin Doucet <benjamin.doucet@ynov.com>
2017-12-19 15:38:10 +0100

    Exercise 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,6 @@
 exports.run = function(input) {
-  console.log("Exercise #3");
+    obj1=Object.values(input[0]);
+    obj2=Object.values(input[1]);
+    ans={firstname: obj2[0], name:obj1[0], money : (obj2[1]+obj1[1])}
+    return ans;
 };
